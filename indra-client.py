68de473d365c22cad0f839da1d72277db311a271
ffff <ffff@localhost.local>
2014-08-04 14:37:17 -0400

    todos

--- indra-client.py
+++ indra-client.py
@@ -1,6 +1,10 @@
-import entropy
+# (c) 2014 ffff (http://cosmopol.is) 
+# MIT license 
+
+from entropy import compute_entropy
 import mindwave-mobile as mw
 
+# TODO: package a json with user id and payload (e.g., type:entropy)
 
 def main():
 
@@ -9,15 +13,18 @@ def main():
     #logging.basicConfig(level=logging.DEBUG)
 
     for pkt in mw.ThinkGearProtocol('/dev/tty.MindWaveMobile-DevA').get_packets():
+
         for d in pkt:
 
             # compute entropy
             if isinstance(d, mw.ThinkGearRawWaveData): 
                 raw_log.append(d)
                 if len(raw_log) > 512:
-                    ent = entropy.compute_entropy(raw_log)
-                    print ent
+                    ent = compute_entropy(raw_log)
+                    print ent # TODO: ship entropy value to server
                     raw_log = []
+
+            # TODO: EEG_POWER business
 
 
 if __name__ == '__main__':
